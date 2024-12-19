06b8b91a9567585fade9da0e79576a3337071eb3
kostaszafiro <kostas.zafiro@gmail.com>
2024-12-19 23:04:19 +0200

    zaf adminpage.css veltiosi

--- gym-management-frontend/src/components/AdminPage.js
+++ gym-management-frontend/src/components/AdminPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import '../styles/AdminPage.css';
 import { BrowserRouter as Router, Route, Routes, Link, } from 'react-router-dom';
 const AdminPage = () =>  {
     return (
